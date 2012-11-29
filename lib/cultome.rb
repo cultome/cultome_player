0bf106fe842c4ed4647a43e90f87662dd7422454
Carlos Soria <zooria@gmail.com>
2012-11-29 16:55:57 -0600

    Specs updated

--- lib/cultome.rb
+++ lib/cultome.rb
@@ -1,10 +1,27 @@
+require 'user_input'
 
 class CultomePlayer
+  include UserInput
+
   def initialize
     listener = PlayerListener.new(self)
-    @player = Player.new()
+    @player = Player.new(listener)
   end
 
   def start
+    puts "Iniciando!" # aqui poner una frase humorisitca aleatoria
+  end
+
+  def execute(user_input)
+    cmds = parse(user_input)
+    cmds.each do |cmd|
+      send(cmd[:command], cmd[:params])
+    end
+  end
+
+  def search(params)
+    return [] if params.empty?
+
+    
   end
 end
