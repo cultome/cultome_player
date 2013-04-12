eb536e0cfff35115d838741bb72b12502f6e4c2d
Carlos Soria <zooria@gmail.com>
2013-04-12 10:06:09 -0500

    Lyric finder plugin added

---
+++ lib/commands/lyric_finder.rb
@@ -0,0 +1,34 @@
+require 'open-uri'
+require 'json'
+require 'cgi'
+require 'htmlentities'
+
+class LyricFinder
+	def initialize(player)
+		@p = player
+	end
+
+	def display(msg)
+		@p.display(ms)
+	end
+
+	def get_command_registry
+		{lyric: {help: "Find the lyric of the current song", params_format: ""}}
+	end
+
+	def lyric(params=[])
+		display("Finding lyric for #{@p.song.name}")
+		response = open("http://lyrics.wikia.com/api.php?artist=#{CGI::escape(@p.artist.name)}&song=#{CGI::escape(@p.song.name)}&fmt=json").string
+		json = JSON.parse(response.gsub("\n", '').gsub("'", '"').gsub('song = ', ''))
+
+		open(json['url']).readlines.each do |line|
+			if line =~ /<div class='lyricbox'>/
+				lyric = HTMLEntities.new.decode(line.gsub(/<div.*?>.*?<\/div>/, '').gsub(/<br.*?>/, "\n").gsub(/<.*/, ''))
+				display(":::: Lyric for #{@p.song.name} ::::")
+				display(lyric)
+				return 
+			end
+		end
+	end
+end
+
