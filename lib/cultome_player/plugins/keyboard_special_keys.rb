03705b6f63f6d0672df74a38adee4fcce4268fa1
Carlos Soria <cultome@protonmail.com>
2016-07-26 23:43:48 -0500

    Change the way the command pipe read the commands

--- lib/cultome_player/plugins/keyboard_special_keys.rb
+++ lib/cultome_player/plugins/keyboard_special_keys.rb
@@ -4,15 +4,16 @@ module CultomePlayer
       def init_plugin_keyboard_special_keys
         Thread.new do
           start_cmd = "tail -f #{command_pipe}"
-          pipe = IO.popen(start_cmd, "rw")
-          pipe.each do |line|
-          #IO.popen(start_cmd).each do |line|
-            # planchamos el prompt...
-            display_over("")
-            # ejecutamos el comando
-            execute_interactively line
-            # y luego lo regeneramos
-            display_over c5(CultomePlayer::Interactive::PROMPT)
+          IO.popen(start_cmd, "r+") do |pipe|
+            begin
+              line = pipe.gets
+              # planchamos el prompt...
+              display_over("")
+              # ejecutamos el comando
+              execute_interactively line
+              # y luego lo regeneramos
+              display_over c5(CultomePlayer::Interactive::PROMPT)
+            end while true
           end # IO
         end # Thread
       end
